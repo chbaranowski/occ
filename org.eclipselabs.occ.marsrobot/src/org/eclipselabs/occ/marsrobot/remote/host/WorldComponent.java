5486f4697a309b8690c4cd199e1c1bc53b02309d
FvUndRaecher@gmail.com <FvUndRaecher@gmail.com@478667a0-1f44-0fd7-74fd-6ef28ad24eee>
2012-09-09 17:25:54 +0000

    Fix configuration admin logic - When the logic was called from a remote bundle the first time you get a strange exception - Fix the bundle location of the wall bundle is now used to save the configuration

--- org.eclipselabs.occ.marsrobot/src/org/eclipselabs/occ/marsrobot/remote/host/WorldComponent.java
+++ org.eclipselabs.occ.marsrobot/src/org/eclipselabs/occ/marsrobot/remote/host/WorldComponent.java
@@ -2,12 +2,13 @@ package org.eclipselabs.occ.marsrobot.remote.host;
 
 import java.io.IOException;
 import java.util.Dictionary;
-import java.util.Hashtable;
 
 import org.eclipselabs.occ.marsrobot.remote.WorldService;
+import org.osgi.framework.BundleContext;
 import org.osgi.service.cm.Configuration;
 import org.osgi.service.cm.ConfigurationAdmin;
 
+import aQute.bnd.annotation.component.Activate;
 import aQute.bnd.annotation.component.Component;
 import aQute.bnd.annotation.component.Reference;
 
@@ -19,8 +20,15 @@ import aQute.bnd.annotation.component.Reference;
 })
 public class WorldComponent implements WorldService {
 
+	private static final String WALL_PID = "org.eclipselabs.occ.marsrobot.wall.WallComponent";
 	ConfigurationAdmin configurationAdmin;
+	private String bundleLocation;
 
+	@Activate
+	public void activate(BundleContext context){
+		bundleLocation = context.getBundle().getLocation();
+	}
+	
 	@Reference
 	public void setConfigurationAdmin(ConfigurationAdmin configurationAdmin) {
 		this.configurationAdmin = configurationAdmin;
@@ -31,37 +39,25 @@ public class WorldComponent implements WorldService {
 			int rotate) {
 		System.out.println("Service creates new wall using ConfigurationAdmin");
 		try {
-			Configuration configuration = getFactoryConfiguration();
+			Configuration configuration = configurationAdmin.createFactoryConfiguration(WALL_PID, bundleLocation);
+			configuration.update();
 			System.out.println("PID is: " + configuration.getPid());
-
-			Dictionary<String, Integer> wallProperties = toDictionary(x, y, z,
-					length, height, rotate);
-			configuration.update(wallProperties);
+			
+			@SuppressWarnings("unchecked")
+			Dictionary<String, Object> wallProps = configuration.getProperties();
+			wallProps.put("x", x);
+			wallProps.put("y", y);
+			wallProps.put("z", z);
+			wallProps.put("length", length);
+			wallProps.put("height", height);
+			wallProps.put("rotate", rotate);
+			configuration.update(wallProps);
+			
 			System.out.println("Service created new wall");
 		} catch (IOException e) {
 			System.out.println("Error on creating configuration for Wall.");
 			e.printStackTrace();
 		}
-	}
-
-	private Configuration getFactoryConfiguration() throws IOException {
-		Configuration configuration = configurationAdmin
-				.createFactoryConfiguration(
-						"org.eclipselabs.occ.marsrobot.wall.WallComponent",
-						null);
-		return configuration;
-	}
-
-	private Dictionary<String, Integer> toDictionary(int x, int y, int z,
-			int length, int height, int rotate) {
-		Dictionary<String, Integer> wallProps = new Hashtable<String, Integer>();
-		wallProps.put("x", x);
-		wallProps.put("y", y);
-		wallProps.put("z", z);
-		wallProps.put("length", length);
-		wallProps.put("height", height);
-		wallProps.put("rotate", rotate);
-		return wallProps;
 	}
 
 }
